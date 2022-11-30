251ca0e746fbeba86ae03dd22768ab25e3518304
Connor Chen <67431889+connorjchen@users.noreply.github.com>
2022-11-29 23:47:58 -0500

    Change to deployed api (#45)

--- client/src/redux/constants.js
+++ client/src/redux/constants.js
@@ -1,6 +1,6 @@
-export const apiBaseUrl = "http://localhost:5000/api";
-// export const apiBaseUrl =
-//   "https://nlaol79agi.execute-api.us-east-1.amazonaws.com/dev/api";
+// export const apiBaseUrl = "http://localhost:5000/api";
+export const apiBaseUrl =
+  "https://nlaol79agi.execute-api.us-east-1.amazonaws.com/dev/api";
 
 export const s3BaseUrl = "https://detrove-s3-images.s3.amazonaws.com";
 export const s3Object = (key) => {
